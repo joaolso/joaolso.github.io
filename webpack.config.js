e9b30ab68f2646d3dd5843a1d7245d2c70f05f08
joaolso <lucas.xms@gmail.com>
2020-07-26 00:19:45 -0300

    adicionado webpack for manager css and js

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+const glob = require('glob');
+const globAll = require('glob-all');
+const PurgecssPlugin = require('purgecss-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  devtool:'source-map',
+  entry:{
+    main: path.join(__dirname, '_webpack', 'main'),
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, './assets/css/')
+  },
+  module: {
+    rules:[{
+       test: /\.(scss)$/,
+       exclude: /node_modules/,
+       use: ExtractTextPlugin.extract({
+         fallback: 'style-loader',
+         use: ['css-loader','sass-loader']
+       })
+    }]
+  },
+  plugins: [
+    new ExtractTextPlugin({
+      filename: 'main.css'
+    }),
+
+    new OptimizeCssAssetsPlugin({
+      assetNameRegExp: /\.css$/g,
+      cssProcessor: require('cssnano'),
+      cssProcessorOptions: {map: {inline:false, annotation:true}},
+      cssProcessorPluginOptions: {
+        preset: ['default', {discardComments: {removeAll: true}}]
+      },
+      canPrint:true
+    }),
+
+    new PurgecssPlugin({
+      paths: globAll.sync([
+        '*.html',
+        './_layouts/*.html',
+        './_includes/*.html',
+        './pages/*.html'
+      ]),
+    })
+  ]
+}
