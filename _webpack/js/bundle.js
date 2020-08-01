2d869876ba85a4c900f5311503e15d58b784255e
joaolso <lucas.xms@gmail.com>
2020-07-31 21:53:01 -0300

    add google analytics

--- _webpack/js/bundle.js
+++ _webpack/js/bundle.js
@@ -1,21 +1,29 @@
-window.onscroll = function () {
-  idExists = document.getElementById("myBar")
-  if (idExists){
-    myFunction();
+window.addEventListener("scroll", scrollFunction);
+
+function scrollFunction() {
+  const backToTopButton = document.querySelector("#back-to-top-btn");
+  if (backToTopButton) {
+    if (window.pageYOffset > 300) {
+      // Show backToTopButton
+      if (!backToTopButton.classList.contains("btnEntrance")) {
+        backToTopButton.classList.remove("btnExit");
+        backToTopButton.classList.add("btnEntrance");
+        backToTopButton.style.display = "block";
+      }
+    } else {
+      // Hide backToTopButton
+      if (backToTopButton.classList.contains("btnEntrance")) {
+        backToTopButton.classList.remove("btnEntrance");
+        backToTopButton.classList.add("btnExit");
+        setTimeout(function () {
+          backToTopButton.style.display = "none";
+        }, 250);
+      }
+    }
   }
-  else {
+  else{
     //pass
   }
-};
-
-function myFunction() {
-  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height =
-    document.documentElement.scrollHeight -
-    document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 100;
-  document.getElementById("myBar").style.width = scrolled + "%";
-  console.log("bar note detect");
 }
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -24,7 +32,6 @@ document.addEventListener("DOMContentLoaded", function () {
     document.querySelectorAll(".navbar-burger"),
     0
   );
-
   // Check if there are any nav burgers
   if ($navbarBurgers.length > 0) {
     // Add a click event on each of them
@@ -41,3 +48,22 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 });
+
+window.onscroll = function () {
+  idExists = document.getElementById("myBar")
+  if (idExists){
+    barProgress();
+  }
+  else {
+    //pass
+  }
+};
+
+function barProgress() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
